3baf31f5ebc68f42bf06eef93a88b8eb54caceab
daikinexana <daiki.yoshioka@nexanahq.com>
2025-10-03 21:37:05 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -152,7 +152,7 @@ export default function Home() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {features.map((feature, _) => (
+            {features.map((feature) => (
               <Link
                 key={feature.name}
                 href={feature.href}
